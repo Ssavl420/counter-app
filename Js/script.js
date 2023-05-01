bfaeb0ebe151f4857eab321a1b9d0959744c3164
Sergey Savlevich <sa.savlevich@gmail.com>
2023-05-02 03:50:07 +0700

    create form

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,24 @@
+const form = document.querySelector('.welcome__form')
+const counterBody = document.querySelector('.counter__inner')
+const welcomeBody = document.querySelector('.welcome__inner')
+const userName = document.querySelector('.name')
+const timeout = 3000;
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const nameForm = document.querySelector('.form__name').value;
+  form.classList.add('-waiting'), timeout;
+  welcomeBody.classList.add('-close'), timeout;
+  counterBody.classList.add('-open');
+  userName.innerText = nameForm;
+});
+
+
+// document.forms.form.onsubmit = function() {
+//   nameForm = this.nameForm.value;
+//   console.log(nameForm)
+//   return false;
+// };
+
 let counter = 0;
 const btnScore = document.querySelector('.counter__addition')
 const btnReset = document.querySelector('.counter__reset')
