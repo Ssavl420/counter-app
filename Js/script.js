69e14609136ed14ec5a9577ef445e693f7e44605
Sergey Savlevich <sa.savlevich@gmail.com>
2023-05-02 17:34:32 +0700

    some class name chanje to js-

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,17 @@
 const form = document.querySelector('.welcome__form')
-const counterBody = document.querySelector('.counter__inner')
 const welcomeBody = document.querySelector('.welcome__inner')
-const userName = document.querySelector('.name')
+const counterBody = document.querySelector('.counter__inner')
+const userName = document.querySelector('.js-name')
 const timeout = 3000;
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   const nameForm = document.querySelector('.form__name').value;
-  welcomeBody.classList.add('-waiting');
+  welcomeBody.classList.add('js-waiting');
   setTimeout(() => {
-  welcomeBody.classList.add('-close');
-  counterBody.classList.add('-open');
+  welcomeBody.classList.add('js-close');
+  counterBody.classList.add('js-open');
   userName.innerText = nameForm;
-  welcomeBody.classList.remove('-waiting');
+  welcomeBody.classList.remove('js-waiting');
   }, 500);
 });
 
@@ -25,8 +25,8 @@ btnScore.addEventListener('click', function () {
   counter = counter + 1;
   score.innerText = counter;
 
-  btnReset.classList.add("-open")
-  btnReset.classList.remove('-disable')
+  btnReset.classList.add("js-open")
+  btnReset.classList.remove('js-disable')
   // if (btnReset = document.querySelector('-disable')) {
   //   btnReset.classList.remove('-disable')};
 });
@@ -34,11 +34,11 @@ btnScore.addEventListener('click', function () {
 btnReset.addEventListener('click', function () {
   if (+cashOut.innerHTML > 0) {cashOut.innerText = +cashOut.innerHTML + counter;
   } else { cashOut.innerText = counter; }
-  document.querySelector('.reset__text').classList.add('-open');
+  document.querySelector('.reset__text').classList.add('js-open');
   counter = 0;
   score.innerText = counter;
 
   // btnReset.classList.remove("-open")
-  btnReset.classList.add("-disable")
+  btnReset.classList.add("js-disable")
 });
 
